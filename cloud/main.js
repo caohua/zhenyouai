2af12ee0cc4434d0902837001a4a4ef5a94921db
caohua <317282606@qq.com>
2013-12-12 09:47:34 +0800

    zhen

--- cloud/main.js
+++ cloud/main.js
@@ -6,5 +6,6 @@ AV.Cloud.define("hello", function(request, response) {
 });
 
 AV.Cloud.define('login', function(request, response){
+	var user = AV.Object.extend("User");
 	response.success({username:'admin',password:'123456'});
 });
