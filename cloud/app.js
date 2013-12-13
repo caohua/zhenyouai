30cf18f0846035f4bbdf30ee15d8fc1341e86864
caohua <317282606@qq.com>
2013-12-13 09:58:44 +0800

    zhen

--- cloud/app.js
+++ cloud/app.js
@@ -8,16 +8,20 @@ app.set('view engine', 'ejs');    // 设置template引擎
 app.use(express.bodyParser());    // 读取请求body的中间件
 
 //使用express路由API服务/hello的http GET请求
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+app.get('/index', function(req, res) {
+  res.render('index', {userName:'阿树1'});
 });
 
 app.get('/login',function(req,res){
-	res.render('login',{userName:'阿树'});
+	res.render('login',{});
+});
+
+app.get('/wedding',function(req,res){
+	res.render('wedding',{userName:'阿树1'});
 });
 
 app.get('/',function(req,res){
-	res.render('index',{});
+	res.render('index',{userName:'阿树1'});
 });
 //最后，必须有这行代码来使express响应http请求
 app.listen();
