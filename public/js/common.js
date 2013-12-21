0d6e3cdd305414ece9d300c9f8957f4e2bcc4c35
caohua <317282606@qq.com>
2013-12-21 13:28:11 +0800

    ssss

---
+++ public/js/common.js
@@ -0,0 +1,13 @@
+﻿$('.btn').bind('click',function(){
+    var txt = $(this).text();
+    var load = $(this).attr('data-loading-text');
+	if (load){
+		$(this).text(load);
+		$(this).attr('disabled','disabled');
+		var btn = $(this);
+		setTimeout(function(){
+			btn.removeAttr('disabled');
+			btn.text(txt);
+		},3000);
+	}
+});
